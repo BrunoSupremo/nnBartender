18a4100241c0a0aa171368106f14381eb38c0007
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2020-02-20 11:19:51 -0300

    Completado

--- javascript.js
+++ javascript.js
@@ -1,45 +1,24 @@
 window.onload = function() {
 
-	//controla a transparencia da barra no topo de acordo com o movimento da tela
+	let barrinhas = document.getElementById("tres_barrinhas");
 	window.onscroll=function(){
-		if( document.querySelectorAll("#menu")[0].classList.contains("menu_aberto") ){
-			document.querySelectorAll("#tres_barrinhas")[0].classList.toggle("fixar_no_topo",
-				document.body.scrollTop >= 29);
-		}else{
-			document.querySelectorAll("#tres_barrinhas")[0].classList.toggle("fixar_no_topo",
-				document.body.scrollTop >= window.innerHeight);
-			document.querySelectorAll("#tres_barrinhas")[0].classList.toggle("visivel",
-				document.body.scrollTop >= window.innerHeight);
-		}
+		//controla a transparencia do menu
+		barrinhas.classList.toggle("fundo_visivel",
+			document.body.scrollTop >= window.innerHeight -90);
 	}
-	//fim do controle de transparencia
 
-
-	//clicou nas barrinhas ou em um link dentro do menu
-	for(i=0; i<document.querySelectorAll("#tres_barrinhas, nav a").length; i++){
-		document.querySelectorAll("#tres_barrinhas, nav a")[i].onclick=function(){
-			document.querySelectorAll("#tres_barrinhas")[0].classList.toggle("em_x");
-			if( document.querySelectorAll("#menu")[0].classList.toggle("menu_aberto") ){
-				document.querySelectorAll("#tres_barrinhas")[0].classList.remove("visivel");
+	let menu = document.getElementById("menu");
+	let links = menu.children;
+	for(let i = 0; i < links.length; i++){
+		links[i].onclick=function(){
+			if (barrinhas.innerHTML == "≡"){
+				barrinhas.innerHTML = "×";
+				menu.classList.toggle("menu_aberto");
 			}else{
-				window.onscroll();
+				barrinhas.innerHTML = "≡";
+				menu.classList.toggle("menu_aberto");
 			}
 		}
 	}
-	//fim do clique das barrinhas
 
-
-	//clicou nas fotos da galeria modal
-	for(i=0; i<document.querySelectorAll("#galeria_modal .imagem_galeria").length; i++){
-		document.querySelectorAll("#galeria_modal .imagem_galeria")[i].onclick=function(){
-			var div = document.createElement("div");
-			div.classList.add("javascript");
-			this.appendChild(div);
-			div.onclick=function(){
-				this.parentNode.removeChild(this);
-				event.stopPropagation(); //pra evitar clicar aqui e ativar o click do pai junto
-			}
-		}
-	}
-	//fim do clique nas fotos
 };
