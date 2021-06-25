31290b09323020091764be20f471e23403c4f682
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2021-06-25 01:37:00 -0300

    hamburg só em tela pequena. menu refeito

--- javascript.js
+++ javascript.js
@@ -1,24 +1,6 @@
 window.onload = function() {
-
-	let barrinhas = document.getElementById("tres_barrinhas");
-	window.onscroll=function(){
-		//controla a transparencia do menu
-		barrinhas.classList.toggle("fundo_visivel",
-			document.body.scrollTop >= window.innerHeight -90);
+	let menu = document.querySelector("nav");
+	menu.onclick=function(){
+		menu.classList.toggle("menu_aberto");
 	}
-
-	let menu = document.getElementById("menu");
-	let links = menu.children;
-	for(let i = 0; i < links.length; i++){
-		links[i].onclick=function(){
-			if (barrinhas.innerHTML == "≡"){
-				barrinhas.innerHTML = "×";
-				menu.classList.toggle("menu_aberto");
-			}else{
-				barrinhas.innerHTML = "≡";
-				menu.classList.toggle("menu_aberto");
-			}
-		}
-	}
-
 };
